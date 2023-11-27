f4009654468fd9eb7663689d8d08a84b6346a5b6
David Lima <antdavidlima@gmail.com>
2023-11-26 21:20:18 -0300

    change BigInt prototype so it can be searialized

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  BigInt.prototype['toJSON'] = function() {
+    return this.toString();
+  };
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
